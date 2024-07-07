79b96d376aea6a8ca86fd9abd76186c4ff6e1114
dariennouri <dan9232@nyu.edu>
2024-07-07 01:10:43 +0000

    update package name. bump to 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dash-ec2-wrapper',
-    version='0.1.0',
+    version='0.1.1',
     description='A custom Dash class for deployment on AWS EC2 instances',
     author='Darien Nouri',
     author_email='nouri.darien@gmail.com',
